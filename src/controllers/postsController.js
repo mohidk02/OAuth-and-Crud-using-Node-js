ac6a3552f912297256fc2af4cfec692cdd960bbb
mohidk02 <mohidasim@gmail.com>
2022-11-02 19:38:21 +0500

    Node js application with OAuth and Crud Restful APIs

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,45 +1,45 @@
 const { posts } = require("../models/posts");
 
-exports.getAllPosts = async (req, res, next) => {
-  posts
-    .findAll({
-      raw: true,
-    })
-    .then((posts) => {
-      res.json({ data: posts });
-    })
-    .catch(function(err) {
-      console.log(err);
-    });
+exports.getAllPosts = async(req, res, next) => {
+    posts
+        .findAll({
+            raw: true,
+        })
+        .then((posts) => {
+            res.json({ data: posts });
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
 };
 
-exports.getUserPost = async (req, res, next) => {
-  posts
-    .findAll({
-      raw: true,
-      where: {
-        id: req.params.id,
-      },
-    })
-    .then((posts) => {
-      res.json({ data: posts });
-    })
-    .catch(function(err) {
-      console.log(err);
-    });
+exports.getUserPost = async(req, res, next) => {
+    posts
+        .findAll({
+            raw: true,
+            where: {
+                id: req.params.id,
+            },
+        })
+        .then((posts) => {
+            res.json({ data: posts });
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
 };
 
-exports.addUserPost = async (req, res, next) => {
-  posts
-    .create({
-      state: req.body.state,
-      description: req.body.description,
-      created_by_id: req.body.created_by_id,
-    })
-    .then((posts) => {
-      res.json(posts);
-    })
-    .catch(function(err) {
-      console.log(err);
-    });
+exports.addUserPost = async(req, res, next) => {
+    posts
+        .create({
+            state: req.body.state,
+            description: req.body.description,
+            created_by_id: req.body.created_by_id,
+        })
+        .then((posts) => {
+            res.json(posts);
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
 };
--- src/models/posts.js
+++ src/models/posts.js
@@ -7,38 +7,40 @@ const REJECTED_STATE = "rejected";
 
 // define posts model
 const posts = con.define("posts", {
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true,
-    unique: true,
-  },
-  state: {
-    type: DataTypes.STRING,
-    validate: {
-      isIn: [[ACTIVE_STATE, ARCHIVED_STATE, REJECTED_STATE]],
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+        unique: true,
     },
-  },
-  created_by_id: {
-    type: DataTypes.INTEGER,
-    defaultValue: 100050,
-    validate: {
-      isNumeric: true,
+    state: {
+        type: DataTypes.STRING,
+        validate: {
+            isIn: [
+                [ACTIVE_STATE, ARCHIVED_STATE, REJECTED_STATE]
+            ],
+        },
     },
-  },
-  created_at: {
-    type: DataTypes.DATEONLY,
-    defaultValue: con.literal("CURRENT_TIMESTAMP"),
-  },
-  updated_at: {
-    type: DataTypes.DATEONLY,
-    defaultValue: con.literal("CURRENT_TIMESTAMP"),
-  },
+    created_by_id: {
+        type: DataTypes.INTEGER,
+        defaultValue: 100050,
+        validate: {
+            isNumeric: true,
+        },
+    },
+    created_at: {
+        type: DataTypes.DATEONLY,
+        defaultValue: con.literal("CURRENT_TIMESTAMP"),
+    },
+    updated_at: {
+        type: DataTypes.DATEONLY,
+        defaultValue: con.literal("CURRENT_TIMESTAMP"),
+    },
 });
 
 posts.addHook("beforeCreate", (posts, options) => {
-  console.log("Inserting records");
+    console.log("Inserting records");
 });
 
 module.exports.posts = posts;
