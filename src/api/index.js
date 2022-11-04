80c3214eb16dec42dc1a6b61fdee187f4b7c0331
Mohid Khan <mohidasim@gmail.com>
2022-11-04 21:01:01 +0500

    Added GraphQL functionality

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,9 @@
 // set express js
 const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
 const authRoute = require("../routes/auth");
+const graphqlSchema = require("../graphql/schema");
+const graphqlResolver = require("../graphql/resolver");
 
 // initialize express and set port
 const app = express();
@@ -10,3 +13,12 @@ app.listen(port, () => console.log(`API Server is listening on port ${port}!`));
 app.use(authRoute);
 // import env  - keep in mind to comment dev_api.sh scripts to prevent overwrite in docket images while debugging
 require("dotenv").config();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: graphqlSchema,
+    rootValue: graphqlResolver,
+    graphiql: "true" === process.env.GRAPHQL_IQ,
+  })
+);
---
+++ src/graphql/resolver.js
@@ -0,0 +1,40 @@
+const { users } = require("../models/users");
+const bcrypt = require("bcrypt");
+module.exports = {
+  users: async function() {
+    const allUsers = await users.findAll({ raw: true });
+    return {
+      usersData: allUsers.map((user) => {
+        return {
+          ...user._doc,
+          userid: user.userid,
+          name: user.name,
+          login: user.login,
+          pass: user.pass,
+        };
+      }),
+    };
+  },
+  createUser: async function({ userInput }, req) {
+    const existingUser = await users.findOne({
+      where: { login: userInput.email },
+    });
+    if (existingUser) {
+      const error = new Error("User Already Exists!");
+      throw error;
+    }
+    const hashPw = await bcrypt.hash(userInput.password, 12);
+    const user = new users({
+      login: userInput.email,
+      name: userInput.name,
+      password: hashPw,
+    });
+    const createUser = await user.save();
+    return {
+      ...createUser._doc,
+      id: createUser.userid,
+      email: createUser.login,
+      password: createUser.pass,
+    };
+  },
+};
